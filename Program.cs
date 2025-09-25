c39ea2f493752f6b5a8de8adf79c769e65fe7ed6
juan.a.mata.guzzo <juan.a.mata.guzzo@avanade.com>
2025-09-25 12:24:28 +0200

    metodo adapter e Inyeccion de dependencias

--- Program.cs
+++ Program.cs
@@ -10,5 +10,18 @@ class Program
 
         var soporte = new ServiceProduct("Apple Care", 101.99m, 365);
         WriteLine(soporte.GetDescription());
+
+        // Aplicación de patron ADAPTER mediante un DTO
+        var mouse = new Product("Magic mouse", 85.99m);
+        var productDto = ProductAdapter.ToDto(mouse);
+        WriteLine($"{productDto.Name} - {productDto.Price:C} - Código: {productDto.Code}");
+
+        // Inyección de dependencias
+        ILabelService labelService = new LabelService();
+        var manager = new ProductManager(labelService);
+        var monitor = new Product("Apple Pro Display", 5000.99m);
+        var installation = new ServiceProduct("Instalación de monitor", 20.99m, 1);
+        manager.PrintLabel(monitor);
+        manager.PrintLabel(installation);
     }
 }
